240e364b85c3bc22c0251ec1de4138502fa2574e
vineet1cg <vineet.prajapati.cg@gmail.com>
2025-10-07 12:32:02 +0530

    added answers

--- Questions/6.js
+++ Questions/6.js
@@ -5,3 +5,12 @@ if(temp_celcius<0){
 else if(temp_celcius>=0 && temp_celcius<=15){
 console.log("Very Cold");
 }
+else if(temp_celcius>=16 && temp_celcius<=25){
+console.log("Cold");
+}
+else if(temp_celcius>=26 && temp_celcius<=35){
+console.log("Warm");
+}
+else if(temp_celcius>=36){
+console.log("Hot");
+}
---
+++ Questions/7.js
@@ -0,0 +1,11 @@
+var marks = 85;
+var income = 250000;
+if(marks>=85 && income<=300000){
+    console.log("Full Scholarship");
+}
+else if(marks>=70 && income<=500000){
+    console.log("Half Scholarship");
+}
+else{
+    console.log("Not Eligible For Scholarship");
+}
