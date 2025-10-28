c0a745100c7612450d2badee5af12d3f8b856add
vineet1cg <vineet.prajapati.cg@gmail.com>
2025-10-28 09:41:54 +0100

    post lunch commit

---
+++ Questions/adjsc.js
@@ -0,0 +1,12 @@
+//to make the page dynamic we must use selectors in JavaScript
+// console.log("Hello!");
+const hello = document.getElementById("h1");
+// console.log(body);
+console.log(hello);
+console.log(hello.textContent);
+// h1.textContent="Done Through JS DOM";
+console.log("partition");
+console.log(hello);
+
+const hello2 = document.getElementsByClassName("yo");
+console.log(hello2);
