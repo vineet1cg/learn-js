22a1a3e73e311faca4e55cc9bf5dfa3161ceda70
vineet1cg <vineet.prajapati.cg@gmail.com>
2025-10-30 11:46:01 +0530

    added first game

---
+++ Samir-Sir-Class/Project 1/index.js
@@ -0,0 +1,34 @@
+let counter = 0;
+let maxScore = 0;
+const button = document.getElementById("click");
+const reset = document.getElementById("reset");
+const update = document.getElementById("update-score");
+const prev = document.getElementById("prev");
+const maxUpdate = document.getElementById("max");
+button.addEventListener("click", () => {
+  counter++;
+  update.innerText = counter;
+  localStorage.setItem("counter", counter);
+  // console.log(counter);
+});
+
+reset.addEventListener("click", () => {
+  prev.innerText = localStorage.getItem("counter");
+  counter = 0;
+  localStorage.setItem("counter",counter);
+  update.innerText = counter;
+  // console.log(counter);
+});
+// preffered way is to use innerText insted of innerHTML when changing the text
+// node list and html content
+// see DOM And How It Works
+// How Does The JS Gets The Data From document
+// make a notes for JS From The Begining And Double Down On Basics
+// const obj = {
+//     name:"hello",
+//     age:18,
+//     add:function(a,b){
+//         console.log(a+b)
+//     }
+// };
+// obj.add(5,4);
