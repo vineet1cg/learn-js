12d815a2255986ccb12a3640edcfbe24bdcce478
vineet1cg <vineet.prajapati.cg@gmail.com>
2025-11-14 14:20:54 +0530

    added anubhav-sir-class

---
+++ Anubhav-Sir-Class/functions.js
@@ -0,0 +1,20 @@
+function functioName( a,b
+    // parameters
+){
+    // logic
+    return a+b;
+}
+
+console.log(functioName(1,1)) // calling the function 
+
+
+// types of function => (1) pure functions (2) impure functions
+//                      (1) Normal Functions (2) Arrow Functions
+// high order functions = map ,forEach , reducer
+// immidiately envoked functions => IEEE
+
+
+// important topics -> Hoisting ,{ let , const , var } <= proper definitions 
+
+// token genration -> they are checked in private routes for login / other purposes as well 
+
---
+++ Anubhav-Sir-Class/truty-falsey.js
@@ -0,0 +1,26 @@
+if(undefined){
+    console.log("Truthy");
+} else{
+    console.log("Falsey")
+}
+// to check for truthy and falsey value
+if(null){
+    console.log("Truthy");
+} else{
+    console.log("Falsey")
+}
+// 
+let count = 0;
+let arr = [1,2,3];
+let i = 0;
+
+if(arr[4]){
+    console.log("Truthy");
+} else {
+    console.log("Falsey");
+}
+while(arr[i]==undefined){
+    count++;
+    i++;
+}
+console.log(count);
